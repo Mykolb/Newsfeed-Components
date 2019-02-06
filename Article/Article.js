613d90c0b3f0aeab2e67d495493a80b1a2fc17b7
Mykol Benning <msmykolbenning@gmail.com>
2019-02-06 18:30:09 -0500

    something's not working

--- Article/Article.js
+++ Article/Article.js
@@ -3,21 +3,25 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
+    console.log(this.domElement);
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListeners('click', this.expandArticle.bind(this));
   }
+
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
+    this.expandButton.classList.toggle('expandButton');
+    this.domElement.classList.toggle('toggle-on');
+    console.log(this.domElement);
   }
 }
-
 /* START HERE: 
 
 - Select all classes named ".article" and assign that value to the articles variable.  
@@ -26,4 +30,7 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+articles.forEach(article => new Articles(article));
+ // console.log(article); shows nodelist //
